ee9ae67d31f066ff3ce489c73a23e196e94a5a95
Eva.Kusyakina <eva.kusyakina@gmail.com>
2025-04-18 21:08:54 +0300

    Initial commit

---
+++ src/test/java/CallbackTest.java
@@ -0,0 +1,77 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CallbackTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        //options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestV1() {
+        driver.get("http://localhost:7777");
+
+        WebElement formElement = driver.findElement(By.cssSelector("form"));
+        List<WebElement> inputs = formElement.findElements(By.cssSelector("input"));
+        inputs.get(0).sendKeys("Василий");
+        inputs.get(1).sendKeys("+79270000000");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button")).click();
+        WebElement resultElement = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
+        assertTrue(resultElement.isDisplayed());
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",resultElement.getText().trim());
+        //String text = driver.findElement(By.className("alert-success")).getText();
+        //assertEquals("Ваша заявка успешно отправлена!", text.trim());
+        //driver.findElement(By.cssSelector("[data-test-id='order-succes']")).click();
+        //List<WebElement> elements = driver.findElements(By.className("input"));
+        //elements.get(0).sendKeys("Василий");
+        //elements.get(1).sendKeys("+79270000000");
+        //driver.findElement(By.className("checkbox__box")).click();
+        //driver.findElement(By.className("button")).click();
+        //String text = driver.findElement(By.className("alert-success")).getText();
+        //assertEquals("Ваша заявка успешно отправлена!", text.trim());
+    }
+
+    //@Test
+    //void shouldTestV2() {
+    //  WebElement form = driver.findElement(By.cssSelector("[data-test-id=callback-form]"));
+    //form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
+    // form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
+    //form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+    //  form.findElement(By.cssSelector("[data-test-id=submit]")).click();
+    //String text = driver.findElement(By.className("alert-success")).getText();
+    // assertEquals("Ваша заявка успешно отправлена!", text.trim());
+    //}
+
+}
